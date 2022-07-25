09367fdb642f47c63ad651dd509255998e473bc3
przemk1 <107305766+przemk1@users.noreply.github.com>
2022-07-25 22:44:01 +0200

    Add files via upload
    add files

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '96346715-59b7-4a22-a0ae-780686f5608b';
+export const MODAL_CLOSE_SEC = 2.5;
